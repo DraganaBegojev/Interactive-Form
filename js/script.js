887a57e7111448cda67ec415b076f91035c0e457
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-03-31 09:35:54 +0200

    Job Role Section
    Hide or display  the "Other job role" section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 const nameInput = document.getElementById('name');
+const jobRoleSelect = document.getElementById("title");
+const otherJobRoleInput = document.getElementById("other-job-role");
+
+otherJobRoleInput.style.display = "none";
+
 document.addEventListener('DOMContentLoaded', () => {
   nameInput.focus();
+});
+
+jobRoleSelect.addEventListener('change', (e) => {
+  if (e.target.value === "other") {
+    otherJobRoleInput.style.display = "block";
+  } else {
+    otherJobRoleInput.style.display = "none";
+  }
 });
