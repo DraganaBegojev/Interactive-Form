f6ae146789592b9f7b6ed0c3abb2c5f250264192
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-04-01 13:03:30 +0200

    Payment Info Section
    Display only the selected payment method.The credit card payment option is selected by default.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 const nameInput = document.getElementById('name');
+
+//Other job role elements
 const jobRoleSelect = document.getElementById("title");
 const otherJobRoleInput = document.getElementById("other-job-role");
+
+// T-shirt info elements
 const designSelect = document.getElementById("design");
 const colorSelect = document.getElementById("color");
 
+// Activities elements
+const activitiesFieldset = document.getElementById("activities");
+const totalCostElement = document.getElementById("activities-cost");
+
+// Payment method elements
+const paymentSelect = document.getElementById("payment");
+const creditCardSection = document.getElementById("credit-card");
+const paypalSection = document.getElementById("paypal");
+const bitcoinSection = document.getElementById("bitcoin");
+
+// Setup on page load
 
 document.addEventListener('DOMContentLoaded', () => {
   nameInput.focus();
   otherJobRoleInput.style.display = "none";
   colorSelect.disabled = true;
+  paymentSelect.value = "credit-card"; // Set default payment method to credit card
+  creditCardSection.style.display = "block";
+  paypalSection.style.display = "none";
+  bitcoinSection.style.display = "none";
 });
 
 // otherJobRoleInput is hidden by default and only shown when the user selects "other" from the job role dropdown.
+
 jobRoleSelect.addEventListener('change', (e) => {
   if (e.target.value === "other") {
     otherJobRoleInput.style.display = "block";
@@ -22,6 +42,7 @@ jobRoleSelect.addEventListener('change', (e) => {
 
 
 // When a design is chosen, the color select element is enabled and the options are filtered based on the selected design.
+
 designSelect.addEventListener("change", (e) => {
   const selectedDesign = e.target.value;
   colorSelect.disabled = false;
@@ -35,10 +56,9 @@ designSelect.addEventListener("change", (e) => {
   }
 });
 
+
 // The total cost of the selected activities
 
-const activitiesFieldset = document.getElementById("activities");
-const totalCostElement = document.getElementById("activities-cost");
 let totalCost = 0;
 
 activitiesFieldset.addEventListener('change', (e) => {
@@ -54,3 +74,17 @@ activitiesFieldset.addEventListener('change', (e) => {
   totalCostElement.innerText = `Total: $${totalCost}`;
 });
 
+
+// The payment methods display
+
+paymentSelect.addEventListener("change", (e) => {
+  const selectedPayment = e.target.value;
+
+  // Hide all payment sections by default
+  creditCardSection.style.display = "none";
+  paypalSection.style.display = "none";
+  bitcoinSection.style.display = "none";
+
+  // Show the selected payment section
+  document.getElementById(selectedPayment).style.display = "block";
+});
