620272e8f26e155d05ab7bd82d082b7108455523
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-04-01 08:42:12 +0200

    T-Shirt Info Section
    First, choose the design and then the available colors for that design.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const nameInput = document.getElementById('name');
 const jobRoleSelect = document.getElementById("title");
 const otherJobRoleInput = document.getElementById("other-job-role");
+const designSelect = document.getElementById("design");
+const colorSelect = document.getElementById("color");
 
 
 document.addEventListener('DOMContentLoaded', () => {
   nameInput.focus();
   otherJobRoleInput.style.display = "none";
+  colorSelect.disabled = true;
 });
 
 jobRoleSelect.addEventListener('change', (e) => {
@@ -13,5 +16,18 @@ jobRoleSelect.addEventListener('change', (e) => {
     otherJobRoleInput.style.display = "block";
   } else {
     otherJobRoleInput.style.display = "none";
+  }
+});
+
+designSelect.addEventListener("change", (e) => {
+  const selectedDesign = e.target.value;
+  colorSelect.disabled = false;
+  
+  // Reset the color selection
+  colorSelect.selectedIndex = 0; 
+
+  for (let colorOption of colorSelect.children) {
+    const dataTheme = colorOption.getAttribute("data-theme");
+    colorOption.hidden = selectedDesign !== dataTheme;
   }
 });
